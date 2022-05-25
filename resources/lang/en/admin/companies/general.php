4468b77844b01b0e3be7f1a81f132dc9c2b78d6a
VNRtranphong <67084965+VNRtranphong@users.noreply.github.com>
2022-05-25 17:05:21 +0700

    Update general.php
    1

--- resources/lang/en/admin/companies/general.php
+++ resources/lang/en/admin/companies/general.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'select_company' => 'Select Company',
-    'about_companies' => 'About Companies',
-    'about_companies_description' => '        You can use companies as a simple informative field, or you can use them to restrict asset visibility and availability to users with a specific company by enabling Full Company Support in your Admin Settings.',
+    'select_company' => 'Select Site',
+    'about_companies' => 'About Site',
+    'about_companies_description' => '        You can use sites as a simple informative field, or you can use them to restrict asset visibility and availability to users with a specific company by enabling Full Company Support in your Admin Settings.',
 ];
