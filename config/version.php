57eea739c2e3ca4a86c10c0011d03987c46bd16e
VNRtranphong <67084965+VNRtranphong@users.noreply.github.com>
2022-05-25 17:03:12 +0700

    Update version.php
    mod

--- config/version.php
+++ config/version.php
@@ -2,9 +2,9 @@
 return array (
   'app_version' => 'v6.0.2',
   'full_app_version' => 'v6.0.2 - build 7880-g43c5b412c',
-  'build_version' => '7880',
+  'build_version' => 'TechGLX',
   'prerelease_version' => '',
   'hash_version' => 'g43c5b412c',
   'full_hash' => 'v6.0.2-128-g43c5b412c',
-  'branch' => 'master',
+  'branch' => 'Pre Test',
 );
