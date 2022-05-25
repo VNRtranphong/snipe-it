ec4f5bbc3270bfd2fb8cd29aa596c97f13af5e32
VNRtranphong <67084965+VNRtranphong@users.noreply.github.com>
2022-05-25 17:06:25 +0700

    Update message.php
    1

--- resources/lang/en/admin/companies/message.php
+++ resources/lang/en/admin/companies/message.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'does_not_exist' => 'Company does not exist.',
-    'assoc_users'    => 'This company is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this company and try again. ',
+    'does_not_exist' => 'Site does not exist.',
+    'assoc_users'    => 'This site is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this site and try again. ',
     'create' => [
-        'error'   => 'Company was not created, please try again.',
-        'success' => 'Company created successfully.',
+        'error'   => 'Site was not created, please try again.',
+        'success' => 'Site created successfully.',
     ],
     'update' => [
-        'error'   => 'Company was not updated, please try again',
-        'success' => 'Company updated successfully.',
+        'error'   => 'Site was not updated, please try again',
+        'success' => 'Site updated successfully.',
     ],
     'delete' => [
-        'confirm' => 'Are you sure you wish to delete this company?',
-        'error'   => 'There was an issue deleting the company. Please try again.',
-        'success' => 'The Company was deleted successfully.',
+        'confirm' => 'Are you sure you wish to delete this Site?',
+        'error'   => 'There was an issue deleting the Site. Please try again.',
+        'success' => 'The Site was deleted successfully.',
     ],
 ];
