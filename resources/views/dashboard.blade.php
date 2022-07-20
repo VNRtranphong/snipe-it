52ffdb73c7568e3d6aa34aff6a620cd3185a73a5
VNRtranphong <67084965+VNRtranphong@users.noreply.github.com>
2022-07-20 14:33:58 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -213,7 +213,7 @@
 
                 <table
                     data-cookie-id-table="dashActivityReport"
-                    data-height="600"
+                    data-height="620"
                     data-pagination="false"
                     data-id-table="dashActivityReport"
                     data-side-pagination="server"
